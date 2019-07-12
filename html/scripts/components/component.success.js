0a9da3ac61bd56aff039c83588821171f990e27b
Unknown <steven.chou.second@gmail.com>
2019-07-12 14:18:59 +0800

    final page

--- html/scripts/components/component.success.js
+++ html/scripts/components/component.success.js
@@ -1,24 +1,47 @@
 //Body
 Vue.component('component-success-main', {
-    template: '#template-success-main',
-    props: ['model','culture'],
-
-
-    methods: {
-        // Btn Click
-        goHome: function () {
-            kiosk.API.goToNext('mainMenu');
-        },
-
-       
-        
-    },
-
-    computed: {
-        wording: function () {
-            return kiosk.wording[this.culture].success;
-        },
+  template: '#template-success-main',
+  props: ['model', 'culture'],
+  methods: {
+    // Btn Click
+    goHome: function() {
+      kiosk.API.goToNext('mainMenu');
     }
+  },
+  computed: {
+    wording: function() {
+      return kiosk.wording[this.culture].success;
+    },
+    btnFontSize: function() {
+      let fontSize = null;
+      switch (this.culture) {
+        case 1:
+          fontSize = 24;
+          break;
+        case 3:
+          fontSize = 20;
+          break;
+        case 4:
+          fontSize = 24;
+          break;
+        case 5:
+          fontSize = 20;
+          break;
+        case 6:
+          fontSize = 24;
+          break;
+        case 7:
+          fontSize = 20;
+          break;
+        default:
+          fontSize = 28;
+      }
+
+      return {
+        fontSize: fontSize + 'px'
+      };
+    }
+  }
 });
 
 //Head
